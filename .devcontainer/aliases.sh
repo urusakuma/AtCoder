ffae1acc0cf711574feb73ab8500b10c7a9b3484
urusakuma <urusakuma@gmail.com>
2025-12-15 10:14:52 +0000

    Update 初期設定を他環境に持ち込むための更新

---
+++ .devcontainer/aliases.sh
@@ -0,0 +1,18 @@
+# PyPy3でのテスト実施
+alias test='oj t -c "pypy3 main.py" -d ./tests/'
+# Pythonでのテスト実施
+alias test2='oj t -c "python3 main.py" -d ./tests/'
+
+# PyPy3での解答提出
+alias sb='acc s main.py -- --guess-python-interpreter pypy'
+# Pythonでの解答提出
+alias sb2='acc s main.py'
+
+# コンテストフォルダへ移動
+alias c='cd contest'
+
+# main.pyを開く
+alias o='code main.py'
+
+# 出力確認用
+alias d='python main.py'
